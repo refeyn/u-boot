525db8d19005d7fbbcac720ef9395b9834bdcde7
Gabriel Dalimonte <gabriel.dalimonte@gmail.com>
2025-02-17 13:26:44 -0500

    fs: fat: add rename
    The implementation roughly follows the POSIX specification for
    rename() [1]. The ordering of operations attempting to minimize the chance
    for data loss in unexpected circumstances.
    
    The 'mv' command was implemented as a front end for the rename operation
    as that is what most users are likely familiar with in terms of behavior.
    
    The 'FAT_RENAME' Kconfig option was added to prevent code size increase on
    size-oriented builds like SPL.
    
    [1] https://pubs.opengroup.org/onlinepubs/9799919799/functions/rename.html
    
    Signed-off-by: Gabriel Dalimonte <gabriel.dalimonte@gmail.com>

--- include/fs.h
+++ include/fs.h
@@ -286,6 +286,8 @@ int do_mkdir(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[],
 	     int fstype);
 int do_ln(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[],
 	  int fstype);
+int do_mv(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[],
+	  int fstype);
 
 /*
  * Determine the UUID of the specified filesystem and print it. Optionally it is
