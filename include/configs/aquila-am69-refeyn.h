6d97e565051fd4a240b7ff732a02bfe642a7afad
Matthew Joyce <matthew.joyce@refeyn.com>
2025-03-03 16:13:24 +0000

    Create AM69 variant

---
+++ include/configs/aquila-am69-refeyn.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Configuration header file for Aquila AM69 SoM on Refeyn carriers
+ *
+ * Copyright 2025 Refeyn
+ */
+
+#ifndef __CONFIG_AQUILA_AM69_REFEYN_H
+#define __CONFIG_AQUILA_AM69_REFEYN_H
+
+#include "aquila-am69.h"
+
+#endif /* __CONFIG_AQUILA_AM69_REFEYN_H */
